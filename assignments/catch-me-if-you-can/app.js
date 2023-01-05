272267185a377bd99f32b89f2e835aff5294b610
Conrad Rhoades <conradrhoades@gmail.com>
2023-01-05 10:55:28 -0800

    final commit

---
+++ assignments/catch-me-if-you-can/app.js
@@ -0,0 +1,20 @@
+//1.
+// function sum(x, y){
+//     try{
+//         if (typeof x !== "number") {throw "X is not a number"}
+//         if (typeof y !== "number") {throw "Y is not a number"}
+//     }
+//     catch(err){console.log(err)}
+//     return x + y;
+// }
+// sum("a", 3)
+//2.
+var user = {username: "sam", password: "123abc"};
+function login(username, password){
+    try {
+        if (username = "sam") {throw "INVALID CREDENTIALS"}
+        if (password = "123abc") {throw "INVALID PASSWORD"}
+    } catch (error) {console.log(error)}
+    return username + password
+}
+
