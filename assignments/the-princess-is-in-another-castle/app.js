598564deca4e2a34f2ae336acc2d7b4c6808f546
Conrad Rhoades <conradrhoades@gmail.com>
2023-01-12 10:13:42 -0800

    inital commit

---
+++ assignments/the-princess-is-in-another-castle/app.js
@@ -0,0 +1,10 @@
+class Character{
+    constructor(name, totalCoins, status, hasStar, setName, addCoin ){
+        this.name = name
+        this.coins = totalCoins
+        this.status = status
+        this.star = hasStar
+        this.scharacterName = setName
+        this.addcoin = addCoin
+    }
+}
