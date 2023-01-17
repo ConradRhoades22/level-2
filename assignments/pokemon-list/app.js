7876307e21a5ce7012aebb9da7eb4ee576b64c0d
Conrad Rhoades <conradrhoades@gmail.com>
2023-01-17 09:10:02 -0800

    final commit

--- assignments/pokemon-list/app.js
+++ assignments/pokemon-list/app.js
@@ -1,1 +1,21 @@
 const xhr = new XMLHttpRequest()
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const JSONdata = xhr.responseText
+        let data = JSON.parse(JSONdata)
+        data = data.objects[0].pokemon
+        showData(data)
+    }
+}
+
+function showData(arr){
+    for(let i = 0; i < arr.length; i++){
+        const h3 = document.createElement('h3')
+        h3.textContent = arr[i].name
+        document.body.appendChild(h3)
+    }
+}
